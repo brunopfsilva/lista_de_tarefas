159f871045b020faa691d3153ea26c637084830a
bruno <bruno.pfsilva@hotmail.com>
2019-11-01 23:17:39 +0000

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:lista_de_tarefas/pages/home.page.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Home(),
+));
