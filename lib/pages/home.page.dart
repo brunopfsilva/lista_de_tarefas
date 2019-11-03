cbfef446f7c9e9c335e39c72a4253e1e240e2d61
bruno <bruno.pfsilva@hotmail.com>
2019-11-03 02:42:16 +0000

    obs listview so esta trabalhando com o Expanded

--- lib/pages/home.page.dart
+++ lib/pages/home.page.dart
@@ -6,17 +6,38 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  List _listTarefas = ["Ir ao mercado", "Estudar", "Exercicio do dia"];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("ok"),
+        title: Text("Lista de tarefas"),
         centerTitle: true,
       ),
-      body: Text("ok"),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
-        onPressed: (){},
+      body: Column(children: <Widget>[
+        Expanded(
+          child: ListView.builder(
+
+            itemCount: _listTarefas.length,
+            itemBuilder: (context,index){
+              return ListTile(
+                title: Text(_listTarefas[index]),
+              );
+            }
+          ),
+        ),
+      ]),
+      floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      floatingActionButton: FloatingActionButton.extended(
+        //floatingActionButton: FloatingActionButton(
+        // child: Icon(Icons.add),
+        icon: Icon(Icons.add),
+        label: Text("Add"),
+        elevation: 9,
+        onPressed: () {
+          showDialog(context: null);
+        },
       ),
     );
   }
